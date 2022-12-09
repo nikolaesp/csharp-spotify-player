7c1c83f7a85c5b08cdf313caa8c990ef4ba6bb15
Nikola Krstevski <nikolakrstevskids@gmail.com>
2022-12-09 19:18:44 +0100

    sistemato classe brano

--- Brano.cs
+++ Brano.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace csharp_spotify_player
 {
-    internal class Brano
+    internal class Brano : IContenutoRiproducibile
     {
+        private string  name;
+        private string artist;
+        private int duration;
+
+        public Brano(string name, string artist, int duration)
+        {
+            this.name = name;
+            this.artist = artist;
+            this.duration = duration;
+
+        }
+
+        public string GetName() { return this.name; }
+        public string GetArtist() { return this.artist;}
+        public int GetDuration() { return this.duration;}
+        public void SetName(string name) { this.name = name;}
+        public void SetArtist(string artist) { this.artist = artist;}
+        public void SetDuration(int duration) { this.duration = duration;}
+        public void Pause()
+        {
+          
+            Console.WriteLine("Messo in pausa il brano {$name} di {$artist}.");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Riproduzione del brano {$name} di {$artist}.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Messo in stop il brano {$name} di {$artist} e riavvolto all’inizio del brano");
+        }
     }
 }
--- Podcast.cs
+++ Podcast.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace csharp_spotify_player
 {
-    internal class Podcast
+    internal class Podcast : IContenutoRiproducibile
     {
+        private string name;
+        private string host;
+        private string guests;
+        private int duration;
+
+        public Podcast(string name, string host, string guests, int duration)
+        {
+            this.name = name;
+            this.host = host;
+            this.guests = guests;
+            this.duration = duration;
+        }
+        public string Name { get { return name; } }
+        public string Host { get { return host; } }
+        public string Guests { get { return guests; } }
+        public int Duration { get { return duration; } }
+
+        public void SetName(string name) { this.name = name;}
+        public void SetHost(string host) { this.host = host;}
+        public void SetGuests(string guests) { this.guests = guests;}
+        public void SetDuration(int duration) { this.duration = duration;}
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
